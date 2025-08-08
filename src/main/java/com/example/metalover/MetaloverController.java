9f291469c33e051a224ce6f7259afd9ea2c2f3b5
yhs <hastar1332@naver.com>
2025-08-08 20:17:28 +0900

    테스트

--- src/main/java/com/example/metalover/MetaloverController.java
+++ src/main/java/com/example/metalover/MetaloverController.java
@@ -5,23 +5,31 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "https://metalover.kr")
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class MetaloverController {
 	
 	private final MetaloverService metaloverService;
 	private final PasswordEncoder passwordEncoder;
-
-	@GetMapping("/index")
+	
+	@GetMapping("/api/hello")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
+	
+	@GetMapping("/api/index")
 	public String index(HttpSession session, Model model) {
 		Object loginUser = session.getAttribute("loginUser");
 	    model.addAttribute("loginUser", loginUser);
